b247af72ffdb5a1278644b970f30f0793b0bb5ab
raviv <raviv.rachmiel@gmail.com>
2015-04-09 00:40:12 +0300

    nothing at all

---
+++ rom/contact.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+$("#submit").click(function() {
+var name = $("#name").val();
+var email = $("#email").val();
+var message = $("#message").val();
+var contact = $("#contact").val();
+var subject = $("#subject").val();
+$("#returnmessage").empty(); // To empty previous error/success message.
+// Checking for blank fields.
+if (name == '' || email == '' || contact == '' || subject=='') {
+alert("אנא מלאו את כל השדות");
+} else {
+// Returns successful data submission message when the entered information is stored in database.
+$.post("send.php", {
+name1: name,
+email1: email,
+message1: message,
+contact1: contact
+}, function(data) {
+$("#returnmessage").append(data); // Append returned message to message paragraph.
+if (data == "ההודעה התקבלה, נשתדל ליצור קשר בהקדםהאפשרי") {
+$("#form")[0].reset(); // To reset form fields on success.
+}
+});
+}
+});
+});
---
+++ rom/send.php
@@ -0,0 +1,35 @@
+<?php
+// Fetching Values from URL.
+$name = $_POST['name1'];
+$email = $_POST['email1'];
+$message = $_POST['message1'];
+$contact = $_POST['contact1'];
+$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
+// After sanitization Validation is performed
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+$subject = $name + " - " + $subject ;
+// To send HTML mail, the Content-type header must be set.
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From:' . $email. "\r\n"; // Sender's Email
+$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
+$template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
+. '<br/>ההודעה נשלחה, תודה.<br/><br/>'
+. 'Name:' . $name . '<br/>'
+. 'Email:' . $email . '<br/>'
+. 'Contact No:' . $contact . '<br/>'
+. 'Message:' . $message . '<br/><br/>'
+. 'This is a Contact Confirmation mail.'
+. '<br/>'
+. 'We Will contact You as soon as possible .</div>';
+$sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $template . "</div>";
+// Message lines should not exceed 70 characters (PHP rule), so wrap it.
+$sendmessage = wordwrap($sendmessage, 70);
+// Send mail by PHP Mail Function.
+mail("raviv@rachmiel.co.il", $subject, $sendmessage, $headers);
+echo "ההודעה התקבלה, נשתדל ליצור קשר בהקדםהאפשרי";
+
+} else {
+echo "<span>* אימייל לא תקין *</span>";
+}
+?>
